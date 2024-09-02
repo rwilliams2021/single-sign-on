46b04381bcdca1f4420df0881b2a3f9668c4d733
rwilliams <richyrichw93@gmail.com>
2024-09-02 12:36:01 +0200

    use Spring initializer

---
+++ src/test/java/com/singlesignon/single_sign_on/SingleSignOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singlesignon.single_sign_on;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleSignOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
